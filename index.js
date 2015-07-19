8ed1e95dd1b630834bef57344a764fc640c75b14
Sean Colandrea <Sean@Seans-MBP.home>
2015-07-19 15:46:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var redis = require('redis');
+var client =  redis.createClient(6379, 'fifty-cache.bpc3fn.ng.0001.usw2.cache.amazonaws.com');
+
+http.listen(3000);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	client.get('count', function(err, reply) {
+	    io.emit('donated', reply);
+	});
+
+	client.get('backers', function(err, reply) {
+		io.emit('oneMoreBacker', reply);
+	})
+
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+	});
+
+	socket.on('oneMoreBacker', function(msg) {
+		client.incr('backers', function(err, reply) {
+			io.emit('oneMoreBacker', reply);
+			console.log(reply);
+		});
+	});
+
+	socket.on('donated', function(msg) {
+		
+		client.incrbyfloat('count', 0.5, function(err, reply) {
+	        io.emit('donated', reply);
+	    });
+		
+	});
+
+});
+
+client.on('connect', function() {
+    console.log('connected');
+});
